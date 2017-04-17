20bd4e25f905877cb78bd26a36de231a8aa11f97
Taishi Sako <8beat0405@gmail.com>
2017-04-18 00:16:19 +0900

    initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerMovement : MonoBehaviour {
+
+	[SerializeField]
+	private float m_speedScale = 7.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float vertical      = CrossPlatformInputManager.GetAxis( "Vertical" );
+		float horizontal    = CrossPlatformInputManager.GetAxis( "Horizontal" );
+		Vector3 addPos      = new Vector3( horizontal, vertical, 0.0f ) * m_speedScale;
+		transform.position += addPos * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using Spine.Unity;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace CompleteProject
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        public float speed = 6f;            // The speed that the player will move at.
+
+        Vector3 movement;                   // The vector to store the direction of the player's movement.
+        private Animator animator;                      // Reference to the animator component.
+        Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
+
+		[Header("Graphics")]
+		private SkeletonAnimator skeletonAnimator;
+
+        void Awake ()
+        {
+            // Set up references.
+            playerRigidbody = GetComponent <Rigidbody> ();
+			GameObject grandChild = this.gameObject.transform.GetChild(0).GetChild(0).gameObject;
+			animator = grandChild.GetComponent<Animator> ();
+			skeletonAnimator = grandChild.GetComponent<SkeletonAnimator> ();
+        }
+
+		void Update ()
+		{
+			
+		}
+
+        void FixedUpdate ()
+        {
+            // Store the input axes.
+            float h = CrossPlatformInputManager.GetAxisRaw("Horizontal");
+            float v = CrossPlatformInputManager.GetAxisRaw("Vertical");
+
+            // Move the player around the scene.
+            Move (h, v);
+
+			//flip left or right
+			if (h > 0) {
+				skeletonAnimator.skeleton.flipX = false;
+			} else if (h < 0) {
+				skeletonAnimator.skeleton.flipX = true;
+			}
+
+			if (h == 0 && v == 0) {
+				animator.SetBool ("isWalking", false);
+				var skeleton = skeletonAnimator.skeleton;
+				skeleton.SetToSetupPose ();
+			} else {
+				animator.SetBool ("isWalking", true);
+				var skeleton = skeletonAnimator.skeleton;
+				skeleton.SetToSetupPose ();
+			}
+        }
+
+
+        void Move (float h, float v)
+        {
+            // Set the movement vector based on the axis input.
+            movement.Set (h, 0f, v);
+            
+            // Normalise the movement vector and make it proportional to the speed per second.
+            movement = movement.normalized * speed * Time.deltaTime;
+
+            // Move the player to it's current position plus the movement.
+            playerRigidbody.MovePosition (transform.position + movement);
+        }
+	}
+}
